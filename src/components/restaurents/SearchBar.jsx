fc2a8acc8e3c464a2460c03c77e118acfbbe4d81
Ajay Sinha <ajaysinhaa09@gmail.com>
2023-09-07 16:29:10 +0530

    changed searchbar

--- src/components/restaurents/SearchBar.jsx
+++ src/components/restaurents/SearchBar.jsx
@@ -17,17 +17,17 @@ const SearchBar = () => {
 
     return (
 
-        <div className="text-center my-2">
+        <div className="text-center my-4">
             <input
                 type="text"
-                className="font-medium px-4 w-7/12 lg:w-3/12 py-3 lg:py-3 text-gray-700 focus:outline-none rounded-l-3xl border-[1px] border-r-0 border-gray-300"
+                className="font-medium px-4 w-7/12 lg:w-3/12 py-2  text-gray-700 focus:outline-none rounded-l-3xl border-[1px] border-r-0 border-gray-300"
                 placeholder="Find Your Favorite Restaurent"
                 value={searchText}
                 onChange={(e) => setSearchText(e.target.value)}
             />
             <button
                 type="submit"
-                className="font-medium px-2 py-3 lg:py-3 w-3/12 lg:w-1/12  bg-gray-700 text-white hover:bg-gray-500 rounded-r-3xl border-[1px] border-l-0 border-gray-700 "
+                className="font-medium px-2 py-2 w-3/12 lg:w-1/12  bg-gray-700 text-white hover:bg-gray-500 rounded-r-3xl border-[1px] border-l-0 border-gray-700 "
                 onClick={() => {
                     const data = filterData(searchText, restaurants.allRestaurents)
                     dispatch(getFilteredRestaurents(data))
